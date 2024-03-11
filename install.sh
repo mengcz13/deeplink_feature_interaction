e84afccceaf91dd07f1c74ddfe5c0b0a9b0826b0
mengcz <mengcz95thu@gmail.com>
2024-03-10 21:39:55 -0700

    update env install scripts

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+conda create -n deeplink_feature_interaction "python=3.11"
 conda install -y pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
 conda install -y numpy scipy pandas matplotlib scikit-learn tqdm jupyter jupyterlab pytables autopep8 pylint wandb
+
 conda create -n deeplink_feature_interaction_r r-essentials r-base
+conda install -c r r-energy
