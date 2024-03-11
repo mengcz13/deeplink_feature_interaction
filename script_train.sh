389aabcf7f666ee065e6674fdce4bc3b045ffbb7
mengcz <mengcz95thu@gmail.com>
2024-03-11 01:53:32 -0700

    random search and best params of humanmicrobiome

--- script_train.sh
+++ script_train.sh
@@ -1,3 +1,3 @@
 # human_microbiome
-
-python train_pred.py --dataset_name human_microbiome --model_type MLP --layer_num 2 --hidden_dims "30" --activation ELU --dropout 0.1 --batch_size 16 --epochs 500 --lr 1e-3 --l1_weight 1e-3 --l2_weight 1e-3 --early_stopping_patience 50 --exptag debug
+## best params from random search (vj6wyprv)
+python train_pred.py --dataset_name human_microbiome --model_type MLP --layer_num 2 --hidden_dims 40 --activation ELU --dropout 0.0 --batch_size 64 --epochs 1200 --lr 0.0001 --l1_weight 0.01 --l2_weight 0.0 --early_stopping_patience 50 --exptag human_microbiome_best
